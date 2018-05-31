e56955a2eab72e2a12ae9d61a6d34557d5eb0a7a
sckarolos <https://sckarolos@bitbucket.org>
2018-05-31 22:11:13 +0100

    Add comments
    Comments on js,html,css code

--- source/js/app.js
+++ source/js/app.js
@@ -1,20 +1,14 @@
-
+// Main Controller
 (function () {
     angular.module('CodeTaskApp', []);
 
     angular.module('CodeTaskApp').controller('MainController', function ($scope, cardService) {
-
+          // Set view model
           let vm = this;
 
           vm.deck = cardService;
           vm.newDeck = [];
-
-          console.log('before σαφολ', vm.deck);
-
-          function testDeck() {
-            this.testDeck = cardService;
-          }
-
+ 
           // Array shuffle - Fisher-Yates algorithm
           // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
           // Call the following on ng-click!
@@ -29,30 +23,32 @@
               // Return shuffled deck
               vm.deck = deck;
 
-              console.log('after σαφολ', vm.deck);
-
               return vm.deck;
           }
 
+          // Remove item from the original deck!
           $scope.removeItem = function(index) {
             vm.newDeck.push(vm.deck[index]);
             vm.deck.splice(index, 1);
           }
 
+          // Remove item from new deck and add it to the original one!
           $scope.addItem = function(index) {
             vm.deck.push(vm.newDeck[index]);
             vm.newDeck.splice(index, 1);
           }
 
+          // Call bubble sort algorithm to sort the deck. The algorithm is called 2 times.
+          // First it sorts the cards by suit and then by value!
           $scope.sortDeck = function(deck) {
             bubbleSort(deck, 'suitEnum', function () {
-                bubbleSort(deck, 'valueEnum');
             });
-            console.log('after new sorting', deck);
             vm.newDeck = deck;
+            bubbleSort(deck, 'valueEnum');
             return vm.newDeck;
           }
 
+          // Implement bubbleSort algorithm!
           function bubbleSort(a, par, callback) {
               var swapped;
               do {
@@ -68,8 +64,6 @@
               } while (swapped);
               if (callback) callback();
           }
-
-
 
     });
 })();
